582199f29eda4c45269178507f07cae383ced303
KiksoS <94075405+KiksoS@users.noreply.github.com>
2024-09-24 16:24:05 +0200

    desplazamientos

--- script.js
+++ script.js
@@ -3,24 +3,10 @@ let rightbt = document.querySelector(".rightbt");
 let leftbar = document.querySelector("#leftBar");
 let rightbar = document.querySelector("#rightBar");
 let cards = document.querySelectorAll(".card");
-let ullist = document.querySelectorAll(".menu")
+let ullist = document.querySelectorAll(".menu");
+let wrapper = document.querySelector(".wrapper");
 
 
-
-rightbt.addEventListener("click",function () {
-    console.log("hola")
-    rightbar.classList.toggle("expand")
-
-    if (rightbar.classList.contains("expand")) {
-        ullist[1].style.visibility = 'visible';
-       
-    }else {
-        ullist[1].style.visibility = 'hidden';
-
-    }
-
-    
-});
 
 leftbt.addEventListener("click",function () {
     leftbar.classList.toggle("expand")
@@ -28,12 +14,32 @@ leftbt.addEventListener("click",function () {
     if (leftbar.classList.contains("expand")) {
         leftbar.style.backgroundColor = '#343a40';
         ullist[0].style.visibility = 'visible';
-        
-       
+        leftbar.classList.remove('close');
+        wrapper.style.left = "11em";
     }else {
+        wrapper.style.left = "9.5em";
         ullist[0].style.visibility = 'hidden';
+        leftbar.classList.toggle("close")
 
     }
+    
+});
+
+rightbt.addEventListener("click",function () {
+    console.log("hola")
+    rightbar.classList.toggle("expand")
+
+    if (rightbar.classList.contains("expand")) {
+        ullist[1].style.visibility = 'visible';
+        rightbar.classList.remove('close')
+        
+        wrapper.style.left = "7em";
+    }else {
+        wrapper.style.left = "9.5em";
+        ullist[1].style.visibility = 'hidden';
+        rightbar.classList.toggle("close")
+    }
+
     
 });
 
