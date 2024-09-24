4c463ec1587c06990fca64a76ae7c74f28a54630
KiksoS <94075405+KiksoS@users.noreply.github.com>
2024-09-24 14:56:49 +0200

    terminado

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ let rightbt = document.querySelector(".rightbt");
 let leftbar = document.querySelector("#leftBar");
 let rightbar = document.querySelector("#rightBar");
 let cards = document.querySelectorAll(".card");
+let ullist = document.querySelectorAll(".menu")
 
 
 
 rightbt.addEventListener("click",function () {
     console.log("hola")
     rightbar.classList.toggle("expand")
+
+    if (rightbar.classList.contains("expand")) {
+        ullist[1].style.visibility = 'visible';
+       
+    }else {
+        ullist[1].style.visibility = 'hidden';
+
+    }
+
     
 });
 
 leftbt.addEventListener("click",function () {
     leftbar.classList.toggle("expand")
+
+    if (leftbar.classList.contains("expand")) {
+        leftbar.style.backgroundColor = '#343a40';
+        ullist[0].style.visibility = 'visible';
+        
+       
+    }else {
+        ullist[0].style.visibility = 'hidden';
+
+    }
     
 });
 
